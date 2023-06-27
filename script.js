b65c1b68a20b479b137f7e0bd92b37f010a4e455
ByteBrewerTK <talifkhan1@hotmail.com>
2023-06-27 20:10:02 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.querySelector('#display');
+
+const inputElem = document.querySelectorAll("input:not(#display,.clear, .delete, .equal)");
+
+//For accessing the value of the button clicked
+inputElem.forEach(input => input.addEventListener('click', () => display.value += input.value));
+
+//clearing the value of display
+document.querySelector('.clear').addEventListener('click', ()=>display.value = "");
+
+//deleting one by one element from the last 
+document.querySelector('.delete').addEventListener('click', ()=>display.value = display.value.toString().slice(0, -1));
+
+//Computing the input values
+// eval() is used for computing the arithmatic operations
+document.querySelector('.equal').addEventListener('click', ()=>{
+    if(display.value.length){
+        let ans = eval(display.value);
+        display.value = (!(Number.isInteger(ans))) ? ans.toFixed(2) : ans;
+    }
+        
+});
